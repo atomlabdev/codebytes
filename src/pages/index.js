882e9de106cc01f970153dd68fe917f7fc8931e4
Tom <tom@codebytes.io>
2020-12-12 00:56:05 +0000

    add holding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,39 +2,18 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 
 function IndexPage() {
   return (
     <Layout>
       <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
+        keywords={[`CodeBytes`]}
+        title="Coming Soon"
       />
 
       <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
+        <h1 className="text-2xl mb-2 font-bold">Coming Soon 🏗</h1>
+        <p><a className="text-blue-500" href="mailto:tom@codebytes.io">tom@codebytes.io</a></p>
       </section>
     </Layout>
   );
